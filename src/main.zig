1dff0a234bbb2fbf886b4b72820f81e9e770bc5a
Eric Eastwood <contact@ericeastwood.com>
2023-09-20 17:48:14 -0500

    Copy area with the X Render extension (24 bit)

--- src/main.zig
+++ src/main.zig
@@ -21,6 +21,10 @@ pub const Ids = struct {
     bg_gc: u32 = 0,
     fg_gc: u32 = 0,
     pixmap: u32 = 0,
+    // For use with the X Render extension
+    picture_root: u32 = 0,
+    picture_window: u32 = 0,
+    picture_pixmap: u32 = 0,
 
     pub fn init(root: u32, base: u32) Self {
         var ids = Ids{
@@ -35,6 +39,9 @@ pub const Ids = struct {
         ids.bg_gc = ids.generateMonotonicId();
         ids.fg_gc = ids.generateMonotonicId();
         ids.pixmap = ids.generateMonotonicId();
+        ids.picture_root = ids.generateMonotonicId();
+        ids.picture_window = ids.generateMonotonicId();
+        ids.picture_pixmap = ids.generateMonotonicId();
 
         return ids;
     }
@@ -45,6 +52,33 @@ pub const Ids = struct {
         self._current_id += 1;
         return current_id;
     }
+};
+
+fn checkMessageLengthFitsInBuffer(message_length: usize, buffer_limit: usize) !void {
+    if (message_length > buffer_limit) {
+        std.debug.panic("Reply is bigger than our buffer (data corruption will ensue) {} > {}. In order to fix, increase the buffer size.", .{
+            message_length,
+            buffer_limit,
+        });
+    }
+}
+
+pub fn findMatchingPictureFormat(formats: []const x.render.PictureFormatInfo, desired_depth: u8) !x.render.PictureFormatInfo {
+    for (formats) |format| {
+        if (format.depth != desired_depth) continue;
+        return format;
+    }
+    return error.VisualTypeNotFound;
+}
+
+const ExtensionInfo = struct {
+    extension_name: []const u8,
+    opcode: u8,
+    base_error_code: u8,
+};
+
+const Extensions = struct {
+    render: ExtensionInfo,
 };
 
 pub fn main() !u8 {
@@ -213,12 +247,13 @@ pub fn main() !u8 {
     }
 
     const double_buf = try x.DoubleBuffer.init(
-        std.mem.alignForward(usize, 1000, std.mem.page_size),
+        std.mem.alignForward(usize, 8000, std.mem.page_size),
         .{ .memfd_name = "ZigX11DoubleBuffer" },
     );
-    // double_buf.deinit() (not necessary)
+    defer double_buf.deinit(); // not necessary but good to test
     std.log.info("read buffer capacity is {}", .{double_buf.half_len});
     var buf = double_buf.contiguousReadBuffer();
+    const buffer_limit = buf.half_len;
 
     const font_dims: FontDims = blk: {
         _ = try x.readOneMsg(conn.reader(), @alignCast(buf.nextReadBuffer()));
@@ -252,6 +287,136 @@ pub fn main() !u8 {
         try common.send(conn.sock, &msg);
     }
 
+    {
+        const ext_name = comptime x.Slice(u16, [*]const u8).initComptime("RENDER");
+        var msg: [x.query_extension.getLen(ext_name.len)]u8 = undefined;
+        x.query_extension.serialize(&msg, ext_name);
+        try conn.send(&msg);
+    }
+    _ = try x.readOneMsg(conn.reader(), @alignCast(buf.nextReadBuffer()));
+    const optional_render_extension = blk: {
+        switch (x.serverMsgTaggedUnion(@alignCast(buf.double_buffer_ptr))) {
+            .reply => |msg_reply| {
+                const msg: *x.ServerMsg.QueryExtension = @ptrCast(msg_reply);
+                if (msg.present == 0) {
+                    std.log.info("RENDER extension: not present", .{});
+                    break :blk null;
+                }
+                std.debug.assert(msg.present == 1);
+                std.log.info("RENDER extension: opcode={} base_error_code={}", .{ msg.major_opcode, msg.first_error });
+                std.log.info("RENDER extension: {}", .{msg});
+                break :blk ExtensionInfo{
+                    .extension_name = "RENDER",
+                    .opcode = msg.major_opcode,
+                    .base_error_code = msg.first_error,
+                };
+            },
+            else => |msg| {
+                std.log.err("expected a reply but got {}", .{msg});
+                return 1;
+            },
+        }
+    };
+    const render_extension = optional_render_extension orelse @panic("RENDER extension not found");
+
+    {
+        var msg: [x.render.query_version.len]u8 = undefined;
+        x.render.query_version.serialize(&msg, render_extension.opcode, .{
+            .major_version = 0,
+            .minor_version = 11,
+        });
+        try conn.send(&msg);
+    }
+    _ = try x.readOneMsg(conn.reader(), @alignCast(buf.nextReadBuffer()));
+    switch (x.serverMsgTaggedUnion(@alignCast(buf.double_buffer_ptr))) {
+        .reply => |msg_reply| {
+            const msg: *x.render.query_version.Reply = @ptrCast(msg_reply);
+            std.log.info("RENDER extension: version {}.{}", .{ msg.major_version, msg.minor_version });
+            if (msg.major_version != 0) {
+                std.log.err("xrender extension major version {} too new", .{msg.major_version});
+                return 1;
+            }
+            if (msg.minor_version < 11) {
+                std.log.err("xrender extension minor version {} too old", .{msg.minor_version});
+                return 1;
+            }
+        },
+        else => |msg| {
+            std.log.err("expected a reply but got {}", .{msg});
+            return 1;
+        },
+    }
+
+    {
+        var msg: [x.render.query_pict_formats.len]u8 = undefined;
+        x.render.query_pict_formats.serialize(&msg, render_extension.opcode);
+        try conn.send(&msg);
+    }
+    const message_length = try x.readOneMsg(conn.reader(), @alignCast(buf.nextReadBuffer()));
+    try checkMessageLengthFitsInBuffer(message_length, buffer_limit);
+    const pict_formats_data: ?struct { matching_picture_format: x.render.PictureFormatInfo } = blk: {
+        switch (x.serverMsgTaggedUnion(@alignCast(buf.double_buffer_ptr))) {
+            .reply => |msg_reply| {
+                const msg: *x.render.query_pict_formats.Reply = @ptrCast(msg_reply);
+                std.log.info("RENDER extension: pict formats num_formats={}, num_screens={}, num_depths={}, num_visuals={}", .{
+                    msg.num_formats,
+                    msg.num_screens,
+                    msg.num_depths,
+                    msg.num_visuals,
+                });
+                for (msg.getPictureFormats(), 0..) |format, i| {
+                    std.log.info("RENDER extension: pict format ({}) {any}", .{
+                        i,
+                        format,
+                    });
+                }
+                break :blk .{
+                    .matching_picture_format = try findMatchingPictureFormat(msg.getPictureFormats()[0..], depth),
+                };
+            },
+            else => |msg| {
+                std.log.err("expected a reply but got {}", .{msg});
+                return 1;
+            },
+        }
+    };
+    const matching_picture_format = pict_formats_data.?.matching_picture_format;
+
+    {
+        var msg: [x.render.create_picture.max_len]u8 = undefined;
+        const len = x.render.create_picture.serialize(&msg, render_extension.opcode, .{
+            .picture_id = ids.picture_root,
+            .drawable_id = screen.root,
+            .format_id = matching_picture_format.picture_format_id,
+            .options = .{
+                .subwindow_mode = .include_inferiors,
+            },
+        });
+        try conn.send(msg[0..len]);
+    }
+
+    {
+        var msg: [x.render.create_picture.max_len]u8 = undefined;
+        const len = x.render.create_picture.serialize(&msg, render_extension.opcode, .{
+            .picture_id = ids.picture_window,
+            .drawable_id = ids.window,
+            .format_id = matching_picture_format.picture_format_id,
+            .options = .{},
+        });
+        try conn.send(msg[0..len]);
+    }
+
+    {
+        var msg: [x.render.create_picture.max_len]u8 = undefined;
+        const len = x.render.create_picture.serialize(&msg, render_extension.opcode, .{
+            .picture_id = ids.picture_pixmap,
+            .drawable_id = ids.pixmap,
+            .format_id = matching_picture_format.picture_format_id,
+            .options = .{},
+        });
+        try conn.send(msg[0..len]);
+    }
+
     // Show the window. In the X11 protocol is called mapping a window, and hiding a
     // window is called unmapping. When windows are initially created, they are unmapped
     // (or hidden).
@@ -260,6 +425,10 @@ pub fn main() !u8 {
         x.map_window.serialize(&msg, window_id);
         try conn.send(&msg);
     }
+
+    const extensions = Extensions{
+        .render = render_extension,
+    };
 
     while (true) {
         {
@@ -293,6 +462,12 @@ pub fn main() !u8 {
                     std.log.info("todo: handle a reply message {}", .{msg});
                     return error.TodoHandleReplyMessage;
                 },
+                .ge_generic => |msg| {
+                    // TODO: When handling a generic message, we need to get the rest of the event out
+                    // of the buffer since we've only read the first 32 bytes so far.
+                    std.log.info("todo: handle a GE generic event {}", .{msg});
+                    return error.TodoHandleReplyMessage;
+                },
                 .key_press => |msg| {
                     std.log.info("key_press: keycode={}", .{msg.keycode});
                 },
@@ -304,11 +479,13 @@ pub fn main() !u8 {
                     try captureScreenshotToPixmap(
                         conn.sock,
                         ids,
+                        extensions,
                         screenshot_capture_dims,
                     );
                     try render(
                         conn.sock,
                         ids,
+                        extensions,
                         font_dims,
                         state,
                     );
@@ -326,7 +503,13 @@ pub fn main() !u8 {
                     // too much logging
                     //std.log.info("pointer_motion: {}", .{msg});
                     state.mouse_x = msg.event_x;
-                    try render(conn.sock, ids, font_dims, state);
+                    try render(
+                        conn.sock,
+                        ids,
+                        extensions,
+                        font_dims,
+                        state,
+                    );
                 },
                 .keymap_notify => |msg| {
                     std.log.info("keymap_state: {}", .{msg});
@@ -336,6 +519,7 @@ pub fn main() !u8 {
                     try render(
                         conn.sock,
                         ids,
+                        extensions,
                         font_dims,
                         state,
                     );
@@ -410,6 +594,7 @@ fn renderString(
 fn render(
     sock: std.os.socket_t,
     ids: Ids,
+    extensions: Extensions,
     font_dims: FontDims,
     state: State,
 ) !void {
@@ -466,14 +651,33 @@ fn render(
     //     try common.send(sock, &msg);
     // }
 
+    // {
+    //     var msg: [x.copy_area.len]u8 = undefined;
+    //     x.copy_area.serialize(&msg, .{
+    //         .src_drawable_id = ids.pixmap,
+    //         .dst_drawable_id = ids.window,
+    //         .gc_id = ids.fg_gc,
+    //         .src_x = 0,
+    //         .src_y = 0,
+    //         .dst_x = 200,
+    //         .dst_y = 0,
+    //         .width = screenshot_capture_dims.width,
+    //         .height = screenshot_capture_dims.height,
+    //     });
+    //     try common.send(sock, &msg);
+    // }
+
     {
-        var msg: [x.copy_area.len]u8 = undefined;
-        x.copy_area.serialize(&msg, .{
-            .src_drawable_id = ids.pixmap,
-            .dst_drawable_id = ids.window,
-            .gc_id = ids.fg_gc,
+        var msg: [x.render.composite.len]u8 = undefined;
+        x.render.composite.serialize(&msg, extensions.render.opcode, .{
+            .picture_operation = .over,
+            .src_picture_id = ids.picture_pixmap,
+            .mask_picture_id = 0,
+            .dst_picture_id = ids.picture_window,
             .src_x = 0,
             .src_y = 0,
+            .mask_x = 0,
+            .mask_y = 0,
             .dst_x = 200,
             .dst_y = 0,
             .width = screenshot_capture_dims.width,
@@ -486,6 +690,7 @@ fn render(
 fn captureScreenshotToPixmap(
     sock: std.os.socket_t,
     ids: Ids,
+    extensions: Extensions,
     screenshot_capture_dims: ScreenshotCaptureDims,
 ) !void {
     std.log.debug("captureScreenshotToPixmap", .{});
@@ -513,14 +718,33 @@ fn captureScreenshotToPixmap(
     //     try common.send(sock, &msg);
     // }
 
+    // {
+    //     var msg: [x.copy_area.len]u8 = undefined;
+    //     x.copy_area.serialize(&msg, .{
+    //         .src_drawable_id = ids.root,
+    //         .dst_drawable_id = ids.pixmap,
+    //         .gc_id = ids.copy_from_root_gc,
+    //         .src_x = (3840 / 2) - @divExact(@as(i16, @intCast(screenshot_capture_dims.width)), 2),
+    //         .src_y = (2160 / 2) - @divExact(@as(i16, @intCast(screenshot_capture_dims.height)), 2),
+    //         .dst_x = 0,
+    //         .dst_y = 0,
+    //         .width = screenshot_capture_dims.width,
+    //         .height = screenshot_capture_dims.height,
+    //     });
+    //     try common.send(sock, &msg);
+    // }
+
     {
-        var msg: [x.copy_area.len]u8 = undefined;
-        x.copy_area.serialize(&msg, .{
-            .src_drawable_id = ids.root,
-            .dst_drawable_id = ids.pixmap,
-            .gc_id = ids.copy_from_root_gc,
+        var msg: [x.render.composite.len]u8 = undefined;
+        x.render.composite.serialize(&msg, extensions.render.opcode, .{
+            .picture_operation = .over,
+            .src_picture_id = ids.picture_root,
+            .mask_picture_id = 0,
+            .dst_picture_id = ids.picture_pixmap,
             .src_x = (3840 / 2) - @divExact(@as(i16, @intCast(screenshot_capture_dims.width)), 2),
             .src_y = (2160 / 2) - @divExact(@as(i16, @intCast(screenshot_capture_dims.height)), 2),
+            .mask_x = 0,
+            .mask_y = 0,
             .dst_x = 0,
             .dst_y = 0,
             .width = screenshot_capture_dims.width,
